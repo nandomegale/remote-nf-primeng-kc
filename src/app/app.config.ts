ab413474974285c1c3c07add3af491ac836f1e8c
Luiz Fernando Megale Paiva <nando_megale@hotmail.com>
2025-09-27 17:12:56 -0300

    fix README

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,6 +8,7 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -20,5 +21,6 @@ export const appConfig: ApplicationConfig = {
         preset: Aura,
       },
     }),
+    provideHttpClient(),
   ],
 };
