e529ec17794c659e5f43622dbd4cbc1d867415ae
Luiz Fernando Megale Paiva <nando_megale@hotmail.com>
2025-09-27 16:43:51 -0300

    INITIAL COMMIT

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { tap } from 'rxjs';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterLink, ButtonModule],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('portal-privado-remote');
+  protected readonly title = signal('remote-nf-primeng-kc-remote');
+  readonly #http = inject(HttpClient);
+
+  makeHttpRequest() {
+    this.#http.get('https://pokeapi.co/api/v2/pokemon/ditto').pipe(tap(console.log)).subscribe();
+  }
 }
